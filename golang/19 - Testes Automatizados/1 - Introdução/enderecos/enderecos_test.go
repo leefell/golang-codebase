38febfcf37ed4c442e24480b1c1d05648744bd42
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-04-07 22:02:47 -0300

    testes

--- golang/19 - Testes Automatizados/1 - Introdução/enderecos/enderecos_test.go
+++ golang/19 - Testes Automatizados/1 - Introdução/enderecos/enderecos_test.go
@@ -10,6 +10,13 @@ type cenarioDeTeste struct {
 func TestTipoDeEndereco(t *testing.T) {
 	// O arquivo de teste deve terminar com _test.go
 	// E a funcao de Teste tem que ter 'Test' no inicio
+	// Para fazer todos os testes de um diretório usa 'go test ./...'
+	// Para verificar se um teste está testando a função inteira usa 'go test --cover'
+	// Para criar um arquivo de texto com o resultado do go test --cover usa 'go test --coverprofile nomedoarquivo.txt'
+	// e para ler esse arquivo usa 'go tool cover --func=nomedoarquivo.txt'
+	// existe também o 'go tool cover --html=nomedoarquivo.txt'
+
+	t.Parallel() // esses testes podem rodar em paralelo
 
 	cenariosDeTeste := []cenarioDeTeste{
 		{"Rua ABC", "Rua"},
@@ -31,5 +38,14 @@ func TestTipoDeEndereco(t *testing.T) {
 		if retornoRecebido != cenario.retornoEsperado {
 			t.Errorf("O tipo recebido %s é diferente do esperado %s", retornoRecebido, cenario.retornoEsperado)
 		}
+	}
+}
+
+func TestQualquer(t *testing.T) {
+
+	t.Parallel()
+
+	if 1 > 2 {
+		t.Error("Teste Quebrou!")
 	}
 }
