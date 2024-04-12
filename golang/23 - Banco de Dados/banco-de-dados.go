c7a8b92c8a8331a73bce88fe2143054a84cd665d
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-04-11 21:51:48 -0300

    Erro Resolvido, WSL

--- golang/23 - Banco de Dados/banco-de-dados.go
+++ golang/23 - Banco de Dados/banco-de-dados.go
@@ -9,9 +9,10 @@ import (
 )
 
 func main() {
-	// urlConexao := "usuario:senha@/banco"
+	// Construir a string de conexão.
+	stringConexao := "golang:golang@/testeBanco?charset=utf8&parseTime=True&loc=Local"
 
-	stringConexao := "root:1234@/redesocial?charset=utf8&parseTime=True&loc=Local"
+	// Abrir a conexão com o banco de dados MySQL.
 	db, erro := sql.Open("mysql", stringConexao)
 	if erro != nil {
 		fmt.Println("Dentro do SQL.OPEN")
@@ -19,10 +20,19 @@ func main() {
 	}
 	defer db.Close()
 
+	// Verificar se a conexão com o banco de dados é válida.
 	if erro = db.Ping(); erro != nil {
 		fmt.Println("Dentro do ping")
 		log.Fatal(erro)
 	}
 
 	fmt.Println("Conexão está aberta!")
+
+	linhas, erro := db.Query("select * from usuarios")
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	defer linhas.Close()
+
+	fmt.Println(linhas)
 }
