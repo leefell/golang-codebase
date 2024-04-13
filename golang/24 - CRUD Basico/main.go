5aba39212e71ce3dd338aefecdca849ed1b319dd
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-04-13 13:06:17 -0300

    Funções para API

--- golang/24 - CRUD Basico/main.go
+++ golang/24 - CRUD Basico/main.go
@@ -11,11 +11,13 @@ import (
 
 func main() {
 	// Crud - CREATE, READ, UPDATE, DELETE
-
 	// CREATE - POST | READ - GET | UPDATE - PUT | DELETE - DELETE
+	// {id} -> compilador entende que é um valor variavel
 
 	router := mux.NewRouter()
 	router.HandleFunc("/usuarios", servidor.CriarUsuario).Methods(http.MethodPost)
+	router.HandleFunc("/usuarios", servidor.BuscarUsuarios).Methods(http.MethodGet)
+	router.HandleFunc("/usuarios/{id}", servidor.BuscarUsuario).Methods(http.MethodGet)
 
 	fmt.Println("Escutando na porta 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
--- golang/24 - CRUD Basico/servidor/servidor.go
+++ golang/24 - CRUD Basico/servidor/servidor.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type usuario struct {
@@ -58,4 +61,76 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	// Status Codes
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte(fmt.Sprintf("Usuário inserido com sucesso! ID: %d", idInserido)))
+}
+
+// BuscarUsuarios traz todos os usuários salvos no banco de dados
+func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.Write([]byte("Erro ao conectar com o banco de dados!"))
+	}
+	defer db.Close()
+
+	linhas, erro := db.Query("SELECT * FROM usuarios")
+	if erro != nil {
+		w.Write([]byte("Erro ao buscar os usuários!"))
+	}
+	defer linhas.Close()
+
+	var usuarios []usuario // slice de usuários
+	for linhas.Next() {
+		var usuarioAtual usuario // usuário específico que será adicionado ao slice
+
+		//  							1      		ALEXANDRE         			EMAIL
+		if erro := linhas.Scan(&usuarioAtual.ID, &usuarioAtual.Nome, &usuarioAtual.Email); erro != nil {
+			w.Write([]byte("Erro ao escanear o usuário"))
+			return
+		}
+
+		usuarios = append(usuarios, usuarioAtual)
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	if erro := json.NewEncoder(w).Encode(usuarios); erro != nil {
+		w.Write([]byte("Erro ao converteer os usuários para JSON"))
+		return
+	}
+}
+
+// BuscarUsuarios traz um usuário especifico salvos no banco de dados
+func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+
+	ID, erro := strconv.ParseUint(parametros["id"], 10, 32)
+	if erro != nil {
+		w.Write([]byte("Erro ao converter o parâmetro para inteiro!"))
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.Write([]byte("Erro ao conectar com o banco de dados!"))
+		return
+	}
+
+	linha, erro := db.Query("SELECT * FROM usuarios WHERE id = ?", ID)
+	if erro != nil {
+		w.Write([]byte("Erro ao buscar usuário!"))
+		return
+	}
+
+	var usuario usuario
+	if linha.Next() {
+		if erro := linha.Scan(&usuario.ID, &usuario.Nome, &usuario.Email); erro != nil {
+			w.Write([]byte("Erro ao escanear o usuário"))
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusOK)
+	if erro := json.NewEncoder(w).Encode(usuario); erro != nil {
+		w.Write([]byte("Erro ao converter o usuário para JSON"))
+		return
+	}
 }
